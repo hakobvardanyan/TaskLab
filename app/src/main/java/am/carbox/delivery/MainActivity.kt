46917216b60195588d1269841ced0c4a0cc0e523
hakobvardanyan <hakvardanyan@gmail.com>
2023-03-27 09:52:36 +0300

    MainActivity clean up

--- app/src/main/java/am/carbox/delivery/MainActivity.kt
+++ app/src/main/java/am/carbox/delivery/MainActivity.kt
@@ -1,95 +1,11 @@
 package am.carbox.delivery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlin.random.Random
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        test()
-        test2()
-        test3()
-        test4()
-        test5()
-        test6()
-        test7()
-        test8()
-        test9()
-        test12()
-        test13()
-        test14()
-        test15()
-        test16()
-        test17()
-        test18()
-        test19()
-        test21()
-        test22()
-    }
-
-    fun test(): String {
-        val int = Random.nextInt()
-
-        if (int == 3) return "4"
-        if (int == 4) return "44"
-        if (int == 5) return "455"
-        if (int == 6) return "466"
-        return "Fucked"
-    }
-    fun test2() {
-
-    }
-    fun test3() {
-
-    }
-    fun test4() {
-
-    }
-    fun test5() {
-
-    }
-    fun test6() {
-
-    }
-    fun test7() {
-
-    }
-    fun test8() {
-
-    }
-    fun test9() {
-
-    }
-    fun test12() {
-
-    }
-    fun test13() {
-
-    }
-    fun test14() {
-
-    }
-    fun test15() {
-
-    }
-    fun test16() {
-
-    }
-    fun test17() {
-
-    }
-    fun test18() {
-
-    }
-    fun test19() {
-
-    }
-    fun test21() {
-
-    }
-    fun test22() {
-
     }
 }
