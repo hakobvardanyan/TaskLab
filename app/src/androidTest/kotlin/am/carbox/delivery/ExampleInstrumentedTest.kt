7939956c6aefcce2dbbef97c58173993c083de22
hakobvardanyan <hakvardanyan@gmail.com>
2023-03-31 10:40:01 +0300

    updated gradle version

--- app/src/androidTest/kotlin/am/carbox/delivery/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/am/carbox/delivery/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("am.carbox.delivery", appContext.packageName)
+        assertNotEquals("some.random.string", appContext.packageName)
     }
 }
