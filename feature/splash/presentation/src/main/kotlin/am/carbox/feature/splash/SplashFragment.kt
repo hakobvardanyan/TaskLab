920578ac3c57b26c2f97173d32ec87350f23ed3e
hakobvardanyan <hakvardanyan@gmail.com>
2023-05-20 20:44:44 +0300

    Versions bumped up

--- feature/splash/presentation/src/main/kotlin/am/carbox/feature/splash/SplashFragment.kt
+++ feature/splash/presentation/src/main/kotlin/am/carbox/feature/splash/SplashFragment.kt
@@ -1,10 +1,14 @@
 package am.carbox.feature.splash
 
 import am.carbox.feature.splash.presentation.R
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,5 +34,21 @@ class SplashFragment : Fragment() {
             router.get().navigateToLogin("data")
         }
         viewModel.test(requireActivity())
+    }
+}
+
+class CustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(
+    context,
+    attrs,
+    defStyleAttr
+) {
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.d("::::: ", "$h")
     }
 }
